098e67b2dbaebbadf5cc3683cd5dc28c25abe042
raghadsunjaq0 <raghadsunjaq0@gmail.com>
2020-12-09 17:29:52 +0200

    update [models.py]

--- post/models.py
+++ post/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-#from django.utils import timezone
+from django.utils import timezone
 # Create your models here.
 class poost(models.Model):
     title=models.CharField(max_length=50)
     content=models.TextField(max_length=1000)
-
+    created_at=models.DateTimeField(default=timezone.now)
     
-
+    #image=models.ImageField(upload_to='poost-img/')
+ 
     class Meta:
         verbose_name = ("poost")
         verbose_name_plural = ("poosts")
