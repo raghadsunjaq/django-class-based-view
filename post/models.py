43c82952b39c81454205a25b5c583b7d4599cc0f
raghadsunjaq0 <raghadsunjaq0@gmail.com>
2020-12-09 17:05:02 +0200

    add new app [post]

---
+++ post/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+#from django.utils import timezone
+# Create your models here.
+class poost(models.Model):
+    title=models.CharField(max_length=50)
+    content=models.TextField(max_length=1000)
+
+    
+
+    class Meta:
+        verbose_name = ("poost")
+        verbose_name_plural = ("poosts")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("poost_detail", kwargs={"pk": self.pk})
+
